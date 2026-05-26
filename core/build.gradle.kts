38f9c0de1bb02fb4938d26624746dcc291103d57
iAmForyyDev <iAmForyyDev@gmail.com>
2026-05-26 12:13:58 -0500

    build: bump data-container to 1.2.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,9 +8,8 @@ dependencies {
     api(project(":spi"))
 
     implementation("com.github.ben-manes.caffeine:caffeine:3.2.3")
-
-    implementation("com.github.bytephoria.data-container:api:v1.1.0")
-    implementation("com.github.bytephoria.data-container:binary:v1.1.0")
+    implementation("com.github.bytephoria.data-container:api:v1.2.0")
+    implementation("com.github.bytephoria.data-container:binary:v1.2.0")
     compileOnlyApi("org.yaml:snakeyaml:2.6")
 
 }
